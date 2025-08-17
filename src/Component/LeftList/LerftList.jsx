e36f6daedc7d01ade1d9e82685fbe6dc4895263d
Hadary50 <Hadary465@gmai.com>
2025-08-17 13:02:19 +0300

    added sidebar with toggle animation

--- src/Component/LeftList/LerftList.jsx
+++ src/Component/LeftList/LerftList.jsx
@@ -1,34 +1,64 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { UserContext } from '../../Context/CartContext/UserContext/UserContext'
 import { CategoryContext } from '../../Context/CartContext/CategoryContext'
 import { Link, useNavigate } from 'react-router-dom'
+import '../LeftList/sidebar.css' 
 
-export default function LerftList() {
-  let {userToken,setUserToken}=useContext(UserContext)
-  let {getCategory}=useContext(CategoryContext)
-  let navigate=useNavigate()
- async function getSubCategories(product){
-        let res=await getCategory(product)
-    console.log(res?.data.products);
+export default function LeftList() {
+  let { userToken, setUserToken } = useContext(UserContext)
+  let { getCategory } = useContext(CategoryContext)
+  let navigate = useNavigate()
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  async function getSubCategories(product) {
+    let res = await getCategory(product)
+    console.log(res?.data.products)
   }
-  function logout(){
+
+  function logout() {
     localStorage.removeItem('userToken')
     setUserToken(null)
     navigate('/login')
   }
-  return <>
-  <div className="list-group ">
-  
-  <Link to={'/category'}> <li className="dropdown-item list-group-item list-group-item-action" onClick={()=>getSubCategories('smartPhones')}>smartphones</li></Link>
-  <Link to={'/category'}> <li className="dropdown-item list-group-item list-group-item-action" onClick={()=>getSubCategories('fragrances')}>fragrances</li></Link>
-  <Link to={'/category'}> <li className="dropdown-item list-group-item list-group-item-action" onClick={()=>getSubCategories('furniture')}>furniture</li></Link>
-  <Link to={'/category'}> <li className="dropdown-item list-group-item list-group-item-action" onClick={()=>getSubCategories('groceries')}>groceries</li></Link>
-  <Link to={'/category'}> <li className="dropdown-item list-group-item list-group-item-action" onClick={()=>getSubCategories('home-decoration')}>Home docoration</li></Link>
-  <Link to={'/category'}> <li className="dropdown-item list-group-item list-group-item-action" onClick={()=>getSubCategories('laptops')}>laptops</li></Link>
-  <Link to={'/category'}> <li className="dropdown-item list-group-item list-group-item-action" onClick={()=>getSubCategories('mens-shirts')}>mens-shirts</li></Link>
-  <Link to={'/category'}> <li className="dropdown-item list-group-item list-group-item-action" onClick={()=>getSubCategories('motorcycle')}>motorcycle</li></Link>
-  <Link to={'/category'}> <li className="dropdown-item list-group-item list-group-item-action" onClick={()=>getSubCategories('sunglasses')}>sunglasses</li></Link>
-</div>
-  
-  </>
+
+  return (
+    <>
+     
+      <button className="btn btn-btn-outline-dark m-2 w-75 " onClick={() => setIsOpen(!isOpen)}>
+        ☰ Menu
+      </button>
+
+      {/* السايدبار */}
+      <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+        <Link to={'/category'}>
+          <li className="list-group-item" onClick={() => getSubCategories('smartPhones')}>Smartphones</li>
+        </Link>
+        <Link to={'/category'}>
+          <li className="list-group-item" onClick={() => getSubCategories('fragrances')}>Fragrances</li>
+        </Link>
+        <Link to={'/category'}>
+          <li className="list-group-item" onClick={() => getSubCategories('furniture')}>Furniture</li>
+        </Link>
+        <Link to={'/category'}>
+          <li className="list-group-item" onClick={() => getSubCategories('groceries')}>Groceries</li>
+        </Link>
+        <Link to={'/category'}>
+          <li className="list-group-item" onClick={() => getSubCategories('home-decoration')}>Home decoration</li>
+        </Link>
+        <Link to={'/category'}>
+          <li className="list-group-item" onClick={() => getSubCategories('laptops')}>Laptops</li>
+        </Link>
+        <Link to={'/category'}>
+          <li className="list-group-item" onClick={() => getSubCategories('mens-shirts')}>Mens-shirts</li>
+        </Link>
+        <Link to={'/category'}>
+          <li className="list-group-item" onClick={() => getSubCategories('motorcycle')}>Motorcycle</li>
+        </Link>
+        <Link to={'/category'}>
+          <li className="list-group-item" onClick={() => getSubCategories('sunglasses')}>Sunglasses</li>
+        </Link>
+      </div>
+    </>
+  )
 }
